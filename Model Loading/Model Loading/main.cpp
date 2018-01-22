4db6066dcc695f4a3c57b9cba33c1ae609dea8e2
Michael Quint <mequint2000@gmail.com>
2018-01-21 19:00:44 -0800

    Completed the Loading Models tutorial - now able to load .obj files and efficiently display the model with its associated texture

--- Model Loading/Model Loading/main.cpp
+++ Model Loading/Model Loading/main.cpp
@@ -1,13 +1,18 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#define GLM_ENABLE_EXPERIMENTAL
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
 
 #include <algorithm>
 #include <array>
@@ -18,10 +23,14 @@
 #include <iostream>
 #include <set>
 #include <stdexcept>
+#include <unordered_map>
 #include <vector>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
+
+const std::string MODEL_PATH = "models/chalet.obj";
+const std::string TEXTURE_PATH = "textures/chalet.jpg";
 
 const std::vector<const char*> validationLayers =
 {
@@ -119,32 +128,31 @@ struct Vertex
 
 		return attributeDescriptions;
 	}
+
+	bool operator==(const Vertex& other) const
+	{
+		return pos == other.pos && color == other.color && texCoord == other.texCoord;
+	}
 };
+
+namespace std
+{
+	template<> struct hash<Vertex>
+	{
+		size_t operator()(Vertex const& vertex) const
+		{
+			return ((hash<glm::vec3>()(vertex.pos) ^
+				(hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
+				(hash<glm::vec2>()(vertex.texCoord) << 1);
+		}
+	};
+}
 
 struct UniformBufferObject
 {
 	glm::mat4 model;
 	glm::mat4 view;
 	glm::mat4 proj;
-};
-
-const std::vector<Vertex> vertices = 
-{
-	{{-0.5f, -0.5f, 0.0f }, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-	{{ 0.5f, -0.5f, 0.0f }, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
-	{{ 0.5f,  0.5f, 0.0f }, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
-	{{-0.5f,  0.5f, 0.0f }, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}},
-
-	{{-0.5f, -0.5f, -0.5f }, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-	{{ 0.5f, -0.5f, -0.5f }, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
-	{{ 0.5f,  0.5f, -0.5f }, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
-	{{-0.5f,  0.5f, -0.5f }, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}}
-};
-
-const std::vector<uint16_t> indices = 
-{
-	0, 1, 2, 2, 3, 0,
-	4, 5, 6, 6, 7, 4
 };
 
 class HelloTriangleApplication
@@ -199,14 +207,20 @@ class HelloTriangleApplication
 		VkDescriptorPool descriptorPool;
 		VkDescriptorSet descriptorSet;
 
+		// Texture mapping tutorial add-ons
 		VkImage textureImage;
 		VkDeviceMemory textureImageMemory;
 		VkImageView textureImageView;
 		VkSampler textureSampler;
 
+		// Depth buffering tutorial add-ons
 		VkImage depthImage;
 		VkDeviceMemory depthImageMemory;
 		VkImageView depthImageView;
+
+		// Model loading tutorial add-ons
+		std::vector<Vertex> vertices;
+		std::vector<uint32_t> indices;
 
 		//********** Life cycle methods **********//
 		void initWindow()
@@ -215,7 +229,7 @@ class HelloTriangleApplication
 
 			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 			
-			window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan - Depth Buffering", nullptr, nullptr);
+			window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan - Model Loading", nullptr, nullptr);
 
 			glfwSetWindowUserPointer(window, this);
 			glfwSetWindowSizeCallback(window, HelloTriangleApplication::onWindowResized);
@@ -241,6 +255,7 @@ class HelloTriangleApplication
 			createTextureImageView();
 			createTextureSampler();
 
+			loadModel();
 			createVertexBuffer();
 			createIndexBuffer();
 			createUniformBuffer();
@@ -885,7 +900,7 @@ class HelloTriangleApplication
 			// Load the image file from the file system
 			int texWidth, texHeight, texChannels;
 
-			stbi_uc* pixels = stbi_load("textures/texture.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+			stbi_uc* pixels = stbi_load(TEXTURE_PATH.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
 
 			VkDeviceSize imageSize = texWidth * texHeight * 4;
 
@@ -1104,6 +1119,54 @@ class HelloTriangleApplication
 				0, nullptr, 0, nullptr, 1, &barrier);
 
 			endSingleTimeCommands(commandBuffer);
+		}
+
+		void loadModel()
+		{
+			tinyobj::attrib_t attrib;
+			std::vector<tinyobj::shape_t> shapes;
+			std::vector<tinyobj::material_t> materials;
+			std::string err;
+
+			if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, MODEL_PATH.c_str()))
+			{
+				throw std::runtime_error(err);
+			}
+
+			std::unordered_map<Vertex, uint32_t> uniqueVertices = {};
+
+			for (const auto& shape : shapes)
+			{
+				for (const auto& index : shape.mesh.indices)
+				{
+					Vertex vertex = {};
+
+					vertex.pos =
+					{
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2]
+					};
+
+					// NOTE: The origin for tex-coords is the top-left in Vulkan, must subtract the v-coord from 1
+					//		otherwise the texture looks funky
+					vertex.texCoord =
+					{
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+					};
+
+					vertex.color = { 1.0f, 1.0f, 1.0f };
+
+					if (uniqueVertices.count(vertex) == 0)
+					{
+						uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+						vertices.push_back(vertex);
+					}
+
+					indices.push_back(uniqueVertices[vertex]);
+				}
+			}
 		}
 
 		void createVertexBuffer()
@@ -1367,7 +1430,7 @@ class HelloTriangleApplication
 				VkBuffer vertexBuffers[] = { vertexBuffer };
 				VkDeviceSize offsets[] = { 0 };
 				vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
-				vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+				vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT32);
 				vkCmdBindDescriptorSets(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 
 					0, 1, &descriptorSet, 0, nullptr);
 
